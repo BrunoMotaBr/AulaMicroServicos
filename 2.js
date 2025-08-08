ed441c792abbde465c47e784646b8434f89d71a6
BrunoMotaBr <bruno.silvamota@outlook.com>
2025-08-08 06:59:31 -0300

    Teste 3

--- 2.js
+++ 2.js
@@ -1,1 +1,14 @@
-console.log("Teste")
+function test(a, b){
+    return a + b
+}
+
+test(2,3)
+5
+
+const valor1 = 5
+let valor2 = 5  /*
+    Diferenças entre var e let => 
+        let é dentro de um escopo fica 
+        limitado lá dentro o var é do programa todo
+*/
+var valor3 = 5
